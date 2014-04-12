237dcf9bd60ccead25859d13e8fe00d30519c9a3
TJ Holowaychuk <tj@vision-media.ca>
2014-04-12 12:17:11 -0700

    add test for unmapped param

--- test.js
+++ test.js
@@ -8,9 +8,9 @@ describe('paramify', function(){
   route = paramify(route);
   var param = route.param;
   var get = route.get;
-  
+
   var app = koa();
-  
+
   param('user', function*(id, next){
     if (id == '1') {
       this.user = { name: 'one' };
@@ -19,40 +19,40 @@ describe('paramify', function(){
       this.status = 404;
     }
   });
-  
+
   param('user', function*(id, next){
     this.pre = 'lol: ';
     yield next;
   });
-  
+
   app.use(get('/:user', function*(){
     this.body = this.pre + this.user.name;
   }));
-  
+
   it('should work without params', function(done){
     var app = koa();
     var _ = paramify(route);
     app.use(_.get('/', function*(user){
       this.body = 'home';
     }));
-    
+
     request(app.listen())
     .get('/')
     .expect('home', done);
   });
-  
+
   it('should work without param fns', function(done){
     var app = koa();
     var _ = paramify(route);
     app.use(_.get('/:user', function*(user){
       this.body = user;
     }));
-    
+
     request(app.listen())
     .get('/julian')
     .expect('julian', done);
   });
-  
+
   it('should work with param fns', function(done){
     var app = koa();
     var _ = paramify(route);
@@ -63,12 +63,12 @@ describe('paramify', function(){
     app.use(_.get('/:user', function*(){
       this.body = this.user;
     }));
-    
+
     request(app.listen())
     .get('/julian')
     .expect('julian', done);
   });
-  
+
   it('should work with multiple params', function(done){
     var app = koa();
     var _ = paramify(route);
@@ -83,12 +83,35 @@ describe('paramify', function(){
     app.use(_.get('/:user/:repo', function*(){
       this.body = this.user + ': ' + this.repo;
     }));
-    
+
     request(app.listen())
     .get('/julian/repo')
     .expect('julian: repo', done);
   });
-  
+
+  it('should work with unmapped params', function(done){
+    var app = koa();
+    var _ = paramify(route);
+
+    _.param('user', function*(id, next){
+      this.user = 'julian';
+      yield next;
+    });
+
+    _.param('repo', function*(id, next){
+      this.repo = 'repo';
+      yield next;
+    });
+
+    app.use(_.get('/:user/:repo/:file', function*(user, repo, file){
+      this.body = this.user + ': ' + this.repo + ': ' + file;
+    }));
+
+    request(app.listen())
+    .get('/julian/repo/jquery.js')
+    .expect('julian: repo: jquery.js', done);
+  });
+
   it('should abort inside param fns', function(done){
     var app = koa();
     var _ = paramify(route);
@@ -98,12 +121,12 @@ describe('paramify', function(){
     app.use(_.get('/:user', function*(){
       this.body = this.user;
     }));
-    
+
     request(app.listen())
     .get('/julian')
     .expect(404, done);
   });
-  
+
   it('should accept middleware', function(done){
     var app = koa();
     var _ = paramify(route);
@@ -118,12 +141,12 @@ describe('paramify', function(){
     app.use(_.get('/:user', function*(){
       this.body = this.user;
     }));
-    
+
     request(app.listen())
     .get('/julian')
     .expect('JULIAN', done);
   });
-  
+
   it('should clone', function(){
     assert(route != paramify(route));
   });
